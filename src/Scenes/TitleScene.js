b2ea80c7a4474b574c455b3981d6315b6ebab60a
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-05 22:53:07 +0100

    Configure titleScene

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -1,11 +1,68 @@
 import "phaser";
+import config from "../Config/config";
+import Button from "../Objects/Button";
 
 export default class TitleScene extends Phaser.Scene {
   constructor() {
     super("Title");
   }
 
-  preload() {}
+  create() {
+    // Game
+    this.gameButton = new Button(
+      this,
+      config.width / 2,
+      config.height / 2 - 100,
+      "blueButton1",
+      "blueButton2",
+      "Play",
+      "Game"
+    );
 
-  create() {}
+    // Options
+    this.optionsButton = new Button(
+      this,
+      config.width / 2,
+      config.height / 2,
+      "blueButton1",
+      "blueButton2",
+      "Options",
+      "Options"
+    );
+
+    // Credits
+    this.creditsButton = new Button(
+      this,
+      config.width / 2,
+      config.height / 2 + 100,
+      "blueButton1",
+      "blueButton2",
+      "Credits",
+      "Credits"
+    );
+
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add("bgMusic", { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
+    }
+  }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(
+        config.width / 2,
+        config.height / 2 - offset * 100,
+        config.width,
+        config.height
+      )
+    );
+  }
+
+  centerButtonText(gameText, gameButton) {
+    Phaser.Display.Align.In.Center(gameText, gameButton);
+  }
 }

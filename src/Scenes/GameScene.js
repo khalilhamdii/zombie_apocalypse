7545b30c3250e130c9ad7df682a4791866d0bc23
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-06 00:22:00 +0100

    Add player sprit and create movement animation

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -18,5 +18,44 @@ export default class GameScene extends Phaser.Scene {
 
     // make all tiles in obstacles collidable
     obstacles.setCollisionByExclusion([-1]);
+
+    //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("player", {
+        frames: [1, 7, 1, 13],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // animation with key 'right'
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("player", {
+        frames: [1, 7, 1, 13],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "up",
+      frames: this.anims.generateFrameNumbers("player", {
+        frames: [2, 8, 2, 14],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "down",
+      frames: this.anims.generateFrameNumbers("player", {
+        frames: [0, 6, 0, 12],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // our player sprite created through the phycis system
+    this.player = this.physics.add.sprite(50, 100, "player", 6);
   }
 }

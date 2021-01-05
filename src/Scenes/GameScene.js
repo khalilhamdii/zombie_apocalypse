a13fc28df20bc0b2d9d58c6c2c62570bda051783
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-06 00:32:02 +0100

    add player movement to update

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -93,4 +93,39 @@ export default class GameScene extends Phaser.Scene {
       this
     );
   }
+
+  update(time, delta) {
+    //    this.controls.update(delta);
+
+    this.player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown) {
+      this.player.body.setVelocityX(-80);
+    } else if (this.cursors.right.isDown) {
+      this.player.body.setVelocityX(80);
+    }
+
+    // Vertical movement
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-80);
+    } else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(80);
+    }
+
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (this.cursors.left.isDown) {
+      this.player.anims.play("left", true);
+      this.player.flipX = true;
+    } else if (this.cursors.right.isDown) {
+      this.player.anims.play("right", true);
+      this.player.flipX = false;
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play("up", true);
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play("down", true);
+    } else {
+      this.player.anims.stop();
+    }
+  }
 }

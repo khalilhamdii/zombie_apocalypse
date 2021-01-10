5f928c010c636e1e44c3148f60cfb77a10b787ce
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-10 04:36:50 +0100

    Add more test cases to the highscoreapi  functions

--- src/Objects/HighScoreApi.js
+++ src/Objects/HighScoreApi.js
@@ -1,26 +1,28 @@
 const HighScoreApi = (() => {
-  const addNewScore = (name, score) => {
+  const addNewScore = async (name, score) => {
     let _score = {
       user: `${name}`,
       score: score,
     };
-
-    fetch(
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yRcKnB2kevhO71ry1wUe/scores/",
-      {
-        method: "POST",
-        body: JSON.stringify(_score),
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-      }
-    )
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.log(err));
+    try {
+      const request = await fetch(
+        "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yRcKnB2kevhO71ry1wUe/scores/",
+        {
+          method: "POST",
+          body: JSON.stringify(_score),
+          headers: { "Content-type": "application/json; charset=UTF-8" },
+        }
+      );
+      const response = await request.json();
+      console.log(response);
+    } catch (error) {
+      return error;
+    }
   };
 
   const getTopFive = (obj) => {
     const arr = [];
-    obj["result"].forEach((element) =>
+    obj.forEach((element) =>
       arr.push({ name: element.user, score: element.score })
     );
     const sortedArr = arr.sort((a, b) => parseInt(b.score) - parseInt(a.score));
@@ -33,7 +35,7 @@ const HighScoreApi = (() => {
         "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yRcKnB2kevhO71ry1wUe/scores/"
       );
       const data = await response.json();
-      return getTopFive(data);
+      return getTopFive(data["result"]);
     } catch (error) {
       return error;
     }
--- src/_test_/HighScoreApi.test.js
+++ src/_test_/HighScoreApi.test.js
@@ -1,10 +1,84 @@
 import "babel-polyfill";
 import HighScoreApi from "../Objects/HighScoreApi";
 
-test("Expect AddNewScore to send the score to the API", () => {
-  // HighScoreApi.getTopFive();
+test("Get object from the API", () => {
+  HighScoreApi.getListOfScores().then((scores) => {
+    expect(typeof scores).toBe("object");
+  });
 });
 
-test("Expect getTopFive to return the objects with top 5 scores", () => {});
+test("Get the Top score username", () => {
+  HighScoreApi.getListOfScores().then((scores) => {
+    const username = scores[0].name;
+    expect(username).toBe("Topscore");
+  });
+});
 
-test("Expect getListOfScores to get a list of scores from the API", () => {});
+test("Get the Top score recorded", () => {
+  HighScoreApi.getListOfScores().then((scores) => {
+    const topScore = scores[0].score;
+    expect(topScore).toBe(100000);
+  });
+});
+
+test("Post a valid score of 500", () => {
+  HighScoreApi.addNewScore("Test_1", 500).then((response) => {
+    expect(response.result).toBe("Leaderboard score created correctly.");
+  });
+});
+
+test("Post invalid username", () => {
+  HighScoreApi.addNewScore("", 10).then((response) => {
+    expect(response.result).toBe(undefined);
+  });
+});
+
+test("Return top five object sorted by score value", () => {
+  const objects = [
+    { user: "name1", score: 1 },
+    { user: "name2", score: 2 },
+    { user: "name3", score: 3 },
+    { user: "name4", score: 4 },
+    { user: "name5", score: 5 },
+    { user: "name6", score: 6 },
+    { user: "name7", score: 7 },
+    { user: "name8", score: 8 },
+    { user: "name9", score: 9 },
+    { user: "name10", score: 10 },
+  ];
+
+  const expected = [
+    { name: "name10", score: 10 },
+    { name: "name9", score: 9 },
+    { name: "name8", score: 8 },
+    { name: "name7", score: 7 },
+    { name: "name6", score: 6 },
+  ];
+
+  expect(HighScoreApi.getTopFive(objects)).toEqual(expected);
+});
+
+test("Not to return the scores unsorted", () => {
+  const objects = [
+    { user: "name1", score: 1 },
+    { user: "name2", score: 2 },
+    { user: "name3", score: 3 },
+    { user: "name4", score: 4 },
+    { user: "name5", score: 5 },
+    { user: "name6", score: 6 },
+    { user: "name7", score: 7 },
+    { user: "name8", score: 8 },
+    { user: "name9", score: 9 },
+    { user: "name10", score: 10 },
+  ];
+
+  const expected = [
+    { name: "name6", score: 6 },
+    { name: "name7", score: 7 },
+    { name: "name8", score: 8 },
+    { name: "name9", score: 9 },
+    { name: "name10", score: 10 },
+  ];
+
+  expect(HighScoreApi.getTopFive(objects)).not.toEqual(expected);
+});
